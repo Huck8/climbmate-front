a0042a549d34a6e76f29a5a96cac2f934586f050
Ángel <angelmonclova@gmail.com>
2023-03-16 12:16:13 +0100

    Create login form component

---
+++ src/features/users/LoginForm.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  ButtonFormStyled,
+  InfoRegisterStyled,
+  LoginFormStyled,
+} from './LoginFormStyled';
+import { InputFormStyled } from './LoginFormStyled';
+import { LabelFormStyled } from './LoginFormStyled';
+
+const LoginForm = () => {
+  // const handlerFormStyledHover = () => {
+
+  // };
+  return (
+    <>
+      <LoginFormStyled>
+        <LabelFormStyled htmlFor="email">Email</LabelFormStyled>
+
+        <InputFormStyled
+          type="email"
+          placeholder="Introduce tu email"
+          name="email"
+          id="email"
+          required
+        />
+        <LabelFormStyled htmlFor="password">Contraseña</LabelFormStyled>
+        <InputFormStyled
+          type="password"
+          placeholder="Introduce tu contraseña"
+          name="password"
+          id="password"
+          required
+        />
+        <ButtonFormStyled type="submit">LOGIN</ButtonFormStyled>
+        <InfoRegisterStyled>
+          <p>
+            ¿No tienes cuenta?<span>Regístrate</span>
+          </p>
+        </InfoRegisterStyled>
+      </LoginFormStyled>
+    </>
+  );
+};
+
+export default LoginForm;

f9d139dfd7e11306cc13ddd0afb198acfbde3791
Ángel <angelmonclova@gmail.com>
2023-03-16 12:52:15 +0100

    Change the location of the login pages and login components

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LoginForm from '../../features/users/LoginForm';
+import LoginForm from '../../features/user/components/login-form/LoginForm';
 import { LoginPageStyled } from './LoginPageStyled';
 
 const LoginPage = () => {

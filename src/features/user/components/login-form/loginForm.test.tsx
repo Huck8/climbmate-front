f5037ca88067b452c8a569263d960207f2900d1a
Ángel <angelmonclova@gmail.com>
2023-03-20 20:38:40 +0100

    Modify test and upload

--- src/features/user/components/login-form/loginForm.test.tsx
+++ src/features/user/components/login-form/loginForm.test.tsx
@@ -1,3 +1,58 @@
+// import { render, screen, waitFor } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import { Provider } from 'react-redux';
+// import { store } from '../../../../app/store';
+// import { server } from '../../../../mocks/server';
+// import LoginForm from './LoginForm';
+
+// beforeAll(() => server.listen());
+// afterEach(() => server.resetHandlers());
+// afterAll(() => server.close());
+
+// describe('Given a login form component', () => {
+//   test('When the component loads, then it should be a welcome message', async () => {
+//     render(
+//       <Provider store={store}>
+//         <LoginForm />
+//       </Provider>,
+//     );
+
+//     expect(
+//       await screen.findByText('CLIMB WITH NEW FRIENDS'),
+//     ).toBeInTheDocument();
+//   });
+
+//   test('When a user tries to login with a valid email and password, then he should receive his access token', async () => {
+//     render(
+//       <Provider store={store}>
+//         <LoginForm />
+//       </Provider>,
+//     );
+
+//     await userEvent.type(
+//       screen.getByPlaceholderText('Email'),
+//       'email@test.com',
+//     );
+//     await userEvent.type(screen.getByPlaceholderText('Password'), 'password');
+//     userEvent.click(screen.getByRole('button'));
+//   });
+
+//   test('When there is an error while logging in, then the user should receive an error message as feedback', async () => {
+//     render(
+//       <Provider store={store}>
+//         <LoginForm />
+//       </Provider>,
+//     );
+
+//     await userEvent.type(
+//       screen.getByPlaceholderText('Email'),
+//       'email3@test.com',
+//     );
+//     await userEvent.type(screen.getByPlaceholderText('Password'), 'password');
+//     userEvent.click(screen.getByRole('button'));
+//   });
+// });
+
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
@@ -35,9 +90,13 @@ describe('Given a login form component', () => {
     );
     await userEvent.type(screen.getByPlaceholderText('Password'), 'password');
     userEvent.click(screen.getByRole('button'));
+
+    await waitFor(() => {
+      expect(screen.getByText('Successfully logged in!')).toBeInTheDocument();
+    });
   });
 
-  test('When there is an error while logging in, then the user should receive an error message as feedback', async () => {
+  test('When an user tries to log with incorrect email or password, then it should respond an error', async () => {
     render(
       <Provider store={store}>
         <LoginForm />
@@ -46,7 +105,7 @@ describe('Given a login form component', () => {
 
     await userEvent.type(
       screen.getByPlaceholderText('Email'),
-      'email3@test.com',
+      'email2@test.com',
     );
     await userEvent.type(screen.getByPlaceholderText('Password'), 'password');
     userEvent.click(screen.getByRole('button'));

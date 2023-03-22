5d0b0a71c1b69e3630d8a91664ef526399ae819b
Ángel <angelmonclova@gmail.com>
2023-03-22 12:50:15 +0100

    Fix changes and remove all changes previous

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,15 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { CardListPage } from '../pages/Card-list/CardListPage';
+
 import { LoginPage } from '../pages/Login/LoginPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <LoginPage />,
-  },
-  {
-    path: '/auth',
-    element: <CardListPage />,
   },
 ]);
 

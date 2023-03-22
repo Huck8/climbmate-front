a85288673d4b5cf0f6b299f29ba38401a721503d
Ángel <angelmonclova@gmail.com>
2023-03-22 12:40:10 +0100

    Fix link styled of card list

--- src/pages/Card-list/CardListPage.tsx
+++ src/pages/Card-list/CardListPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ExcursionCard from '../../features/user/components/excursion-card/ExcursionCard';
 
-import { ExcursionCardStyled } from '../../features/user/components/excursion-card/ExcursionCardStyled';
 import { CardListPageStyled } from './CardListPageStyled';
 
 export const CardListPage = () => {

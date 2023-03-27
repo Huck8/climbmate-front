c5d986165ffc4af0d79f62f3fd074f3973c5939f
Ángel <angelmonclova@gmail.com>
2023-03-27 20:24:17 +0200

    Modify conexion to excursion api

--- src/features/excursion/excursion-api.ts
+++ src/features/excursion/excursion-api.ts
@@ -1,7 +1,7 @@
 export const getAllExcursions = async () => {
   const response = await fetch(
-    //`${process.env.REACT_APP_API_URL}/api/v1/excursion`,
-    `http://localhost:4000/api/v1/excursion`,
+    `${process.env.REACT_APP_API_URL}/api/v1/excursion`,
+    // `http://localhost:4000/api/v1/excursion`,
     {
       headers: {
         Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
